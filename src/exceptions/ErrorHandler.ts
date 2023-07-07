e5797b7413e4b2edd3a1d7fa3b2e6f8a676a5a2d
NathansStuff <n.odonnell@hotmail.com>
2023-07-07 17:36:20 +1000

    fix: fix

--- src/exceptions/ErrorHandler.ts
+++ src/exceptions/ErrorHandler.ts
@@ -1,18 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/constants';
-
 import { AuthenticationError } from './AuthenticationError';
 import { BadRequestError } from './BadRequestError';
 import { CustomError } from './CustomError';
 import { NotFoundError } from './NotFoundError';
 
-export function ErrorHandler(err: CustomError, req: NextApiRequest, res: NextApiResponse) {
-  const status = err.status !== null && err.status !== undefined ? err.status : 500;
+export function ErrorHandler(
+  err: CustomError,
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const status =
+    err.status !== null && err.status !== undefined ? err.status : 500;
   const message = err.message.length > 0 ? err.message : 'Something went wrong';
   const error = err.error !== null ? err.error : null;
 
-  if (err instanceof BadRequestError || err instanceof AuthenticationError || err instanceof NotFoundError) {
+  if (
+    err instanceof BadRequestError ||
+    err instanceof AuthenticationError ||
+    err instanceof NotFoundError
+  ) {
     return err;
   }
 
@@ -20,7 +27,7 @@ export function ErrorHandler(err: CustomError, req: NextApiRequest, res: NextApi
     status,
     message,
     error,
-    stack: NEXT_PUBLIC_ENVIRONMENT === 'production' ? '🥞' : err.stack,
+    stack: '🥞',
   };
 
   (res as any).status(status).json(errorResponse);
